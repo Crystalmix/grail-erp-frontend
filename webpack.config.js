61da9f52f89d114e2cbbab10a0cb77171a7c3eee
Egor Yurtaev <yurtaev.egor@gmail.com>
2016-11-25 16:31:53 +0600

    Add source-map

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
   context: path.join(__dirname, 'src'),
+  devtool: 'eval-source-map',
   entry: {
     main: './main.js',
     vendor: [
