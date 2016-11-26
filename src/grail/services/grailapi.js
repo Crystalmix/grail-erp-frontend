c8330be45322ffa5b4bdf122a0c37af3da70364d
Egor Yurtaev <yurtaev.egor@gmail.com>
2016-11-27 03:08:15 +0600

    refactoring grailapi.js

--- src/grail/services/grailapi.js
+++ src/grail/services/grailapi.js
@@ -4,6 +4,7 @@ import type { XeroConst } from '../constants/xero'
 
 export default ($http, configs, auth_status, $state, $exceptionHandler, xero: XeroConst) => {
   const api = {}
+
   api.request = (method, path, body) => $http({
     method,
     url: `${configs.API_URL}/api${path}`,
@@ -42,166 +43,63 @@ export default ($http, configs, auth_status, $state, $exceptionHandler, xero: Xe
     }
   })
 
-  api.login = function (data) {
-    return this.auth_request('POST', '/login/', data)
-  }
+  api.login = (data) => api.auth_request('POST', '/login/', data)
+  api.logout = () => api.auth_request('GET', '/logout/')
+  api.reset = (data) => api.auth_request('POST', '/password/reset/', data)
+  api.registration = (data) => api.auth_request('POST', '/registration/', data)
 
-  api.logout = function () {
-    return this.auth_request('GET', '/logout/')
-  }
+  api.getProfile = () => api.user_profile_request('GET', '/user/')
+  api.updateProfile = (data) => api.user_profile_request('PUT', '/user/', data)
 
-  api.reset = function (data) {
-    return this.auth_request('POST', '/password/reset/', data)
-  }
-
-  api.registration = function (data) {
-    return this.auth_request('POST', '/registration/', data)
-  }
-
-  api.getProfile = function () {
-    return this.user_profile_request('GET', '/user/')
-  }
-
-  api.updateProfile = function (data) {
-    return this.user_profile_request('PUT', '/user/', data)
-  }
-
-  api.getOrganizartions = function () {
-    return this.request('GET', '/organizations/')
-  }
-
-  api.updateOrganization = function (id, data) {
-    return this.request('PUT', `/organizations/${id}/`, data)
-  }
-
+  api.getOrganizartions = () => api.request('GET', '/organizations/')
+  api.updateOrganization = (id, data) => api.request('PUT', `/organizations/${id}/`, data)
   api.getOrganizartionImportXeroUrl = () => `${configs.API_SERVER}/xero/organizations/organization/import/?token=${auth_status.getToken()}`
-
   api.getTaxRatesAndAccountsImportXeroUrl = () => `${configs.API_SERVER}/xero/others/tax_rates_accounts/import/?token=${auth_status.getToken()}`
 
   // Customers and Suppliers
   api.getCustomerExportXeroUrl = id => `${configs.API_SERVER}/xero/contacts/contacts/export/${id}/?token=${auth_status.getToken()}`
-
-  // Customers and Suppliers
-  api.getCustomerExportXeroUrl = id => `${configs.API_SERVER}/xero/contacts/contacts/export/${id}/?token=${auth_status.getToken()}`
-
-  api.getContacts = function () {
-    return this.request('GET', '/contacts/')
-  }
-
-  api.getCustomers = function () {
-    return this.request('GET', '/contacts/?IsCustomer=True')
-  }
-
-  api.getSuppliers = function () {
-    return this.request('GET', '/contacts/?IsSupplier=True')
-  }
-
-  api.updateContact = function (id, data) {
-    return this.request('PUT', `/contacts/${id}/`, data)
-  }
-
-  api.addContact = function (data) {
-    return this.request('POST', '/contacts/', data)
-  }
-
-  api.deleteContactLocation = function (contact_id, location_id) {
-    return this.request('Delete', `/contacts/${contact_id}/delete_location/${location_id}/`)
-  }
-
+  api.getContacts = () => api.request('GET', '/contacts/')
+  api.getCustomers = () => api.request('GET', '/contacts/?IsCustomer=True')
+  api.getSuppliers = () => api.request('GET', '/contacts/?IsSupplier=True')
+  api.updateContact = (id, data) => api.request('PUT', `/contacts/${id}/`, data)
+  api.addContact = (data) => api.request('POST', '/contacts/', data)
+  api.deleteContactLocation = (contact_id, location_id) => api.request('Delete', `/contacts/${contact_id}/delete_location/${location_id}/`)
   api.getContactsImportXeroUrl = () => `${configs.API_SERVER}/xero/contacts/contacts/import/?token=${auth_status.getToken()}`
-
   api.getContactsExportXeroUrl = () => `${configs.API_SERVER}/xero/contacts/contacts/export/?token=${auth_status.getToken()}`
 
   // Warehouses
-  api.getWarehouses = function () {
-    return this.request('GET', '/warehouses/')
-  }
-
-  api.updateWarehouse = function (id, data) {
-    return this.request('PUT', `/warehouses/${id}/`, data)
-  }
-
-  api.addWarehouse = function (data) {
-    return this.request('POST', '/warehouses/', data)
-  }
+  api.getWarehouses = () => api.request('GET', '/warehouses/')
+  api.updateWarehouse = (id, data) => api.request('PUT', `/warehouses/${id}/`, data)
+  api.addWarehouse = (data) => api.request('POST', '/warehouses/', data)
 
   // Products
-  api.getProducts = function () {
-    return this.request('GET', '/products/')
-  }
-
-  api.updateProduct = function (id, data) {
-    return this.request('PUT', `/products/${id}/`, data)
-  }
-
-  api.addProduct = function (data) {
-    return this.request('POST', '/products/', data)
-  }
-
-  api.deactivateProductItem = function (product_id, item_id) {
-    return this.request('POST', `/products/${product_id}/deactivate_item/${item_id}/`)
-  }
-
-  api.activateProductItem = function (product_id, item_id) {
-    return this.request('POST', `/products/${product_id}/activate_item/${item_id}/`)
-  }
+  api.getProducts = () => api.request('GET', '/products/')
+  api.updateProduct = (id, data) => api.request('PUT', `/products/${id}/`, data)
+  api.addProduct = (data) => api.request('POST', '/products/', data)
+  api.deactivateProductItem = (product_id, item_id) => api.request('POST', `/products/${product_id}/deactivate_item/${item_id}/`)
+  api.activateProductItem = (product_id, item_id) => api.request('POST', `/products/${product_id}/activate_item/${item_id}/`)
 
   // Purchases
-  api.getPurchases = function () {
-    return this.request('GET', `/invoices/?Type=${xero.invoice.purchase_type}`)
-  }
-
-  api.getPurchase = function (id) {
-    return this.request('GET', `/invoices/${id}/`)
-  }
-
-  api.updatePurchase = function (id, data) {
-    return this.request('PUT', `/invoices/${id}/`, data)
-  }
-
-  api.addPurchase = function (data) {
-    return this.request('POST', '/invoices/', data)
-  }
-
-  api.deleteInvoiceLineItem = function (invoice_id, item_id) {
-    return this.request('Delete', `/invoices/${invoice_id}/delete_item/${item_id}/`)
-  }
-
+  api.getPurchases = () => api.request('GET', `/invoices/?Type=${xero.invoice.purchase_type}`)
+  api.getPurchase = (id) => api.request('GET', `/invoices/${id}/`)
+  api.updatePurchase = (id, data) => api.request('PUT', `/invoices/${id}/`, data)
+  api.addPurchase = (data) => api.request('POST', '/invoices/', data)
+  api.deleteInvoiceLineItem = (invoice_id, item_id) => api.request('Delete', `/invoices/${invoice_id}/delete_item/${item_id}/`)
   api.getInvoiceExportXeroUrl = id => `${configs.API_SERVER}/xero/invoices/invoices/export/${id}/?token=${auth_status.getToken()}`
 
   // Sales
-
   api.getInvoiceExportXeroUrl = id => `${configs.API_SERVER}/xero/invoices/invoices/export/${id}/?token=${auth_status.getToken()}`
-
-  api.getSaleLastDocId = function () {
-    return this.request('GET', '/invoices/last_doc_id/')
-  }
-
-  api.getSales = function () {
-    return this.request('GET', `/invoices/?Type=${xero.invoice.sale_type}`)
-  }
-
-  api.getSale = function (id) {
-    return this.request('GET', `/invoices/${id}/`)
-  }
-
-  api.updateSale = function (id, data) {
-    return this.request('PUT', `/invoices/${id}/`, data)
-  }
-
-  api.addSale = function (data) {
-    return this.request('POST', '/invoices/', data)
-  }
+  api.getSaleLastDocId = () => api.request('GET', '/invoices/last_doc_id/')
+  api.getSales = () => api.request('GET', `/invoices/?Type=${xero.invoice.sale_type}`)
+  api.getSale = (id) => api.request('GET', `/invoices/${id}/`)
+  api.updateSale = (id, data) => api.request('PUT', `/invoices/${id}/`, data)
+  api.addSale = (data) => api.request('POST', '/invoices/', data)
 
   // Accounts
-  api.getAccounts = function () {
-    return this.request('GET', '/accounts/')
-  }
+  api.getAccounts = () => api.request('GET', '/accounts/')
 
   // Tax Rates
-  api.getTaxRates = function () {
-    return this.request('GET', '/tax_rates/')
-  }
+  api.getTaxRates = () => api.request('GET', '/tax_rates/')
 
   return api
 }
